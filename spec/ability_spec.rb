b8beab3b3424975fd8c195630f94fdfeac45f296
Daniel Garcia Diaz <mancha0703@gmail.com>
2022-12-07 19:29:12 -0500

    ability specs and item specs fixes

--- spec/ability_spec.rb
+++ spec/ability_spec.rb
@@ -1,11 +1,129 @@
+# frozen_string_literal: true
 require "rspec"
 require "pry"
-require_relative "../src/models/character.rb"
+require_relative "../src/models/ability.rb"
+require_relative "../src/models/item.rb"
 
-RSpec.describe Character do
+RSpec.describe Ability do
+  subject { Ability.new(type: ability_type, multiplier: ability_multiplier, item: ability_item) }
 
-  describe '' do
+  describe '#perform' do
+    context 'when is attack' do
+      let(:ability_type) { 'attack' }
+      let(:ability_multiplier) { 1.2 }
 
+      context 'and no weapon is provided' do
+        let(:failed_weapon_perform) do
+          {
+            value: 0.0,
+            type: 'failed',
+            target: 'enemy'
+          }
+        end
+
+        context 'and nothing is provided' do
+          let(:ability_item) { nil }
+
+          it 'failed' do
+            expect(subject.perform).to include_json(failed_weapon_perform)
+          end
+        end
+
+        context 'and a potion is provided' do
+          let(:ability_item) { Item.new(name: 'potion-1', type: Item::POTION, value: 20) }
+
+          it 'failed' do
+            expect(subject.perform).to include_json(failed_weapon_perform)
+          end
+        end
+
+        context 'and an armor is provided' do
+          let(:ability_item) { Item.new(name: 'armor-1', type: Item::ARMOR, value: 120) }
+
+          it 'failed' do
+            expect(subject.perform).to include_json(failed_weapon_perform)
+          end
+        end
+      end
+
+      context 'and a weapon is provided' do
+        let(:ability_item) { Item.new(name: 'sword-1', type: Item::WEAPON, value: 20) }
+        let(:success_weapon_perform) do
+          {
+            value: 24.0,
+            type: 'damage',
+            target: 'enemy'
+          }
+        end
+
+        it 'causes 24 damage' do
+          expect(subject.perform).to include_json(success_weapon_perform)
+        end
+      end
+    end
+
+    context 'when is consumible object' do
+      let(:ability_type) { 'consumible' }
+      let(:ability_multiplier) { 1.2 }
+
+      context 'and no consumible object is provided' do
+        let(:failed_consumible_perform) do
+          {
+            value: 0.0,
+            type: 'failed',
+            target: 'self'
+          }
+        end
+
+        context 'and nothing is provided' do
+          let(:ability_item) { nil }
+
+          it 'failed' do
+            expect(subject.perform).to include_json(failed_consumible_perform)
+          end
+        end
+
+        context 'and a weapon is provided' do
+          let(:ability_item) { Item.new(name: 'weapon-1', type: Item::WEAPON, value: 20) }
+
+          it 'failed' do
+            expect(subject.perform).to include_json(failed_consumible_perform)
+          end
+        end
+      end
+
+      context 'and a consumible object is provided' do
+        context 'and an armor is provided' do
+          let(:ability_item) { Item.new(name: 'armor-1', type: Item::ARMOR, value: 100) }
+          let(:success_consumible_perform) do
+            {
+              value: 120,
+              type: 'armor',
+              target: 'self'
+            }
+          end
+
+          it 'increase armor by 120' do
+            expect(subject.perform).to include_json(success_consumible_perform)
+          end
+        end
+
+        context 'and a potion is provided' do
+          let(:ability_item) { Item.new(name: 'potion-1', type: Item::POTION, value: 20) }
+          let(:success_consumible_perform) do
+            {
+              value: 24.0,
+              type: 'heal',
+              target: 'self'
+            }
+          end
+
+          it 'heals 24 life points' do
+            expect(subject.perform).to include_json(success_consumible_perform)
+          end
+        end
+      end
+    end
   end
 
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,41 +1,23 @@
 # frozen_string_literal: true
-require "rspec"
-require "pry"
-require_relative "../src/models/character.rb"
+require 'rspec'
+require 'pry'
+require_relative '../src/models/character.rb'
 
 RSpec.describe Item do
   subject { Item.new(name: item_name, type: item_type, value: item_value) }
 
   describe 'attributes' do
     context 'when the item is armor' do
-      let(:item_name) { "Armor-1" }
-      let(:item_type) { "armor" }
+      let(:item_name) { 'armor-1' }
+      let(:item_type) { 'armor' }
       let(:item_value) { 100 }
-
-      describe '#name' do
-        expect(subject.name).to eq(item_name)
-      end
-
-      describe '#type' do
-        expect(subject.type).to eq(ITEM::ARMOR)
-      end
-
-      describe '#value' do
-        expect(subject.value).to eq(item_value)
-      end
-    end
-
-    context 'when the item is weapon' do
-      let(:item_name) { "Sword-1" }
-      let(:item_type) { "weapon" }
-      let(:item_value) { 20 }
 
       describe '#name' do
         it { expect(subject.name).to eq(item_name) }
       end
 
       describe '#type' do
-        it { expect(subject.type).to eq(ITEM::WEAPON) }
+        it { expect(subject.type).to eq(Item::ARMOR) }
       end
 
       describe '#value' do
@@ -44,16 +26,34 @@ RSpec.describe Item do
     end
 
     context 'when the item is weapon' do
-      let(:item_name) { "Potion-1" }
-      let(:item_type) { "potion" }
-      let(:item_value) { 10 }
-yo
+      let(:item_name) { 'sword-1' }
+      let(:item_type) { 'weapon' }
+      let(:item_value) { 20 }
+
       describe '#name' do
         it { expect(subject.name).to eq(item_name) }
       end
 
       describe '#type' do
-        it { expect(subject.type).to eq(ITEM::POTION) }
+        it { expect(subject.type).to eq(Item::WEAPON) }
+      end
+
+      describe '#value' do
+        it { expect(subject.value).to eq(item_value) }
+      end
+    end
+
+    context 'when the item is weapon' do
+      let(:item_name) { 'potion-1' }
+      let(:item_type) { 'potion' }
+      let(:item_value) { 10 }
+
+      describe '#name' do
+        it { expect(subject.name).to eq(item_name) }
+      end
+
+      describe '#type' do
+        it { expect(subject.type).to eq(Item::POTION) }
       end
 
       describe '#value' do
@@ -62,16 +62,16 @@ yo
     end
 
     context 'when the type is not armor or weapon' do
-      let(:item_name) { "Other-1" }
-      let(:item_type) { "other" }
+      let(:item_name) { 'other-1' }
+      let(:item_type) { 'other' }
       let(:item_value) { 20 }
 
       describe '#name' do
-        it { expect(subject.name).to eq("unknown") }
+        it { expect(subject.name).to eq('unknown') }
       end
 
       describe '#type' do
-        it { expect(subject.type).to eq(ITEM::UNKNOWN) }
+        it { expect(subject.type).to eq(Item::UNKNOWN) }
       end
 
       describe '#value' do
