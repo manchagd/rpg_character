ade1e3a225361a16fe61b69448107764a4339c9e
Daniel Garcia Diaz <mancha0703@gmail.com>
2022-12-07 21:49:31 -0500

    ability specs fixes and equipment specs

--- spec/ability_spec.rb
+++ spec/ability_spec.rb
@@ -7,6 +7,68 @@ require_relative "../src/models/item.rb"
 RSpec.describe Ability do
   subject { Ability.new(type: ability_type, multiplier: ability_multiplier, item: ability_item) }
 
+  describe 'attributes' do
+    context 'when type is valid' do
+      let(:ability_multiplier) { 1.2 }
+
+      context 'and type is weapon' do
+        let(:ability_type) { 'weapon' }
+        let(:ability_item) { Item.new(name: 'weapon-1', type: Item::WEAPON, value: 20) }
+
+        describe '#type' do
+          it { expect(subject.type).to eq(Ability::WEAPON) }
+        end
+
+        describe '#multiplier' do
+          it { expect(subject.multiplier).to eq(ability_multiplier) }
+        end
+
+        describe '#item' do
+          it { expect(subject.item).to be_instance_of(Item) }
+          it { expect([Item::POTION, Item::ARMOR]).not_to include(subject.item.type) }
+          it { expect(subject.item.type).to eq(Item::WEAPON) }
+        end
+      end
+
+      context 'and type is consumible' do
+        let(:ability_type) { 'consumible' }
+        let(:ability_item) { Item.new(name: 'potion-1', type: Item::POTION, value: 20) }
+
+        describe '#type' do
+          it { expect(subject.type).to eq(Ability::CONSUMIBLE) }
+        end
+
+        describe '#multiplier' do
+          it { expect(subject.multiplier).to eq(ability_multiplier) }
+        end
+
+        describe '#item' do
+          it { expect(subject.item).to be_a(Item) }
+          it { expect([Item::POTION, Item::ARMOR]).to include(subject.item.type) }
+          it { expect(subject.item.type).not_to eq(Item::WEAPON) }
+        end
+      end
+    end
+
+    context 'when type is invalid' do
+      let(:ability_type) { 'other' }
+      let(:ability_multiplier) { 1.2 }
+      let(:ability_item) { Item.new(name: 'potion-1', type: Item::POTION, value: 20) }
+
+      describe '#type' do
+        it { expect(subject.type).to eq(Ability::UNKNOWN) }
+      end
+
+      describe '#multiplier' do
+        it { expect(subject.multiplier).to eq(0) }
+      end
+
+      describe '#item' do
+        it { expect(subject.item).to be_nil }
+      end
+    end
+  end
+
   describe '#perform' do
     context 'when is attack' do
       let(:ability_type) { 'attack' }
--- spec/equipment_spec.rb
+++ spec/equipment_spec.rb
@@ -1,11 +1,103 @@
 require "rspec"
 require "pry"
 require_relative "../src/models/equipment.rb"
+require_relative "../src/models/item.rb"
 
 RSpec.describe Equipment do
+  subject { Equipment.new(size: equipment_size, items: equipment_items) }
+  let(:equipment_size) { 10 }
+  let(:equipment_items) { [] }
 
-  describe '' do
+  describe 'attributes' do
+    describe '#size' do
+      it { expect(subject.size).to eq(equipment_size) }
+    end
+  end
 
+  describe '#available_slots?' do
+    let(:equipment_size) { 1 }
+
+    context 'when is full' do
+      let(:equipment_items) { [Item.new(name: 'potion-1', type: Item::POTION, value: 20)] }
+
+      it 'returns false' do
+        expect(subject.available_slots?).to be_falsey
+      end
+    end
+
+    context 'when has slots' do
+      it 'returns true' do
+        expect(subject.available_slots?).to be_truthy
+      end
+    end
+  end
+
+  describe '#store' do
+    let(:equipment_size) { 1 }
+    let(:item1) { Item.new(name: 'potion-1', type: Item::POTION, value: 20) }
+    let(:item2) { Item.new(name: 'potion-2', type: Item::POTION, value: 20) }
+
+    context 'when is full' do
+      let(:equipment_items) { [item1] }
+
+      it 'returns nil' do
+        expect(subject.store(item2)).to be_nil
+      end
+
+      it 'dont overflow equipment size' do
+        subject.store(item2)
+        expect(subject.size).to eq(1)
+      end
+    end
+
+    context 'when has slots' do
+      it 'returns the item' do
+        expect(subject.store(item2)).to eq(item2)
+      end
+
+      it 'fulfill equipment' do
+        subject.store(item2)
+        expect(subject.size).to eq(1)
+        expect(subject.available_slots?).to be_falsey
+      end
+    end
+  end
+
+  describe '#pick' do
+    let(:equipment_size) { 2 }
+    let(:item1) { Item.new(name: 'potion-1', type: Item::POTION, value: 20) }
+    let(:item2) { Item.new(name: 'potion-2', type: Item::POTION, value: 20) }
+
+    context 'when is full' do
+      let(:equipment_items) { [item1, item2] }
+
+      it 'free one slot' do
+        subject.pick(1)
+        expect(subject.available_slots?).to be_truthy
+      end
+
+      it 'extract and return the first item when pick(1)' do
+        expect(subject.pick(1)).to eq(item1)
+      end
+    end
+
+    context 'when is empty' do
+      it 'returns nil' do
+        expect(subject.pick(1)).to be_nil
+      end
+    end
+  end
+
+  describe '#open' do
+    let(:equipment_size) { 4 }
+    let(:item1) { Item.new(name: 'potion-1', type: Item::POTION, value: 20) }
+    let(:item2) { Item.new(name: 'potion-2', type: Item::POTION, value: 20) }
+    let(:equipment_items) { [item1, item2, nil, nil] }
+
+    it 'shows [item1, nil, nil, nil] after pick(2) ' do
+      subject.pick(2)
+      expect(subject.open).to match_array([item1, nil, nil, nil])
+    end
   end
 
 end
